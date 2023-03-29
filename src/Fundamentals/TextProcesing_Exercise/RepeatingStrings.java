ec0c4ce8b5f425eebc98ca36398d1ee2bf58c317
DiyanIliev96 <diyan.iliev96@gmail.com>
2023-03-30 01:48:05 +0300

    Added text processing task.

---
+++ src/Fundamentals/TextProcesing_Exercise/RepeatingStrings.java
@@ -0,0 +1,21 @@
+package Fundamentals.TextProcesing_Exercise;
+
+import java.util.Scanner;
+
+public class RepeatingStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        StringBuilder text = new StringBuilder(input);
+
+        for (int i = 0; i < text.length() - 1 ; i++) {
+            if (text.charAt(i) == text.charAt(i + 1)) {
+                text.deleteCharAt(i + 1);
+                i--;
+            }
+        }
+        System.out.println(text);
+
+    }
+}
