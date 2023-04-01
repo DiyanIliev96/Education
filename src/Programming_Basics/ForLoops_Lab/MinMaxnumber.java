5a49e165d3e41989125106e74615d632496d82f6
Diyan Iliev <diyan.iliev96@gmail.com>
2023-04-01 22:23:29 +0300

    Added for loop task.

---
+++ src/Programming_Basics/ForLoops_Lab/MinMaxnumber.java
@@ -0,0 +1,23 @@
+package Programming_Basics.ForLoops_Lab;
+
+import java.util.Scanner;
+
+public class MinMaxnumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int maxNumber = Integer.MIN_VALUE;
+        int minNumber = Integer.MAX_VALUE;
+        for (int i = 1; i <= n ; i++) {
+            int currentNum = Integer.parseInt(scanner.nextLine());
+            if (currentNum > maxNumber) {
+                maxNumber = currentNum;
+            }
+            if (currentNum < minNumber) {
+                minNumber = currentNum;
+            }
+        }
+        System.out.printf("Max number: %d%n",maxNumber);
+        System.out.printf("Min number: %d", minNumber);
+    }
+}
