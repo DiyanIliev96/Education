09631e57e8a1dd57a317b2ecdfd7d7d5fc16c65e
Diyan Iliev <diyan.iliev96@gmail.com>
2023-04-03 13:43:26 +0300

    Added for loop task.

---
+++ src/Programming_Basics/ForLoops_Lab/SumLeftAndRightNumbers.java
@@ -0,0 +1,25 @@
+package Programming_Basics.ForLoops_Lab;
+
+import java.util.Scanner;
+
+public class SumLeftAndRightNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 1; i <= n ; i++) {
+            int currentNum = Integer.parseInt(scanner.nextLine());
+            leftSum = leftSum + currentNum;
+        }
+        for (int i = 1; i <= n ; i++) {
+            int currentNum = Integer.parseInt(scanner.nextLine());
+            rightSum = rightSum + currentNum;
+        }
+        if (leftSum == rightSum) {
+            System.out.printf("Yes, sum = %d", leftSum);
+        } else {
+            System.out.printf("No, diff = %d", Math.abs(leftSum - rightSum));
+        }
+    }
+}
