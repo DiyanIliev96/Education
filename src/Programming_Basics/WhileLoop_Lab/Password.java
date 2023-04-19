ab789602ab23ea0431cb05689284e4bbfcc5ccce
Diyan Iliev <diyan.iliev96@gmail.com>
2023-04-19 14:54:46 +0300

    Added while loop task.

---
+++ src/Programming_Basics/WhileLoop_Lab/Password.java
@@ -0,0 +1,16 @@
+package Programming_Basics.WhileLoop_Lab;
+
+import java.util.Scanner;
+
+public class Password {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        String password = scanner.nextLine();
+        String pass = scanner.nextLine();
+        while (!pass.equals(password)) {
+            pass = scanner.nextLine();
+        }
+        System.out.printf("Welcome %s!",username);
+    }
+}
