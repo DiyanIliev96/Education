da7de5f2f7684022e9e6b30be099b36b5432b4f3
Diyan Iliev <diyan.iliev96@gmail.com>
2023-05-04 12:12:11 +0300

    Added while loop task.

---
+++ src/Programming_Basics/WhileLoop_Exercise/num1to100dividedby3.java
@@ -0,0 +1,12 @@
+package Programming_Basics.WhileLoop_Exercise;
+
+public class num1to100dividedby3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <100 ; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
