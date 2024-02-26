8ddb300ba120b359135bc6c335ba91aea8522105
diyaniliev96 <diyan.iliev96@gmail.com>
2024-02-26 19:13:20 +0200

    Task 06 JavaScript homework

---
+++ src/JavaScript Front-End/JS_Syntax/Largest-number.js
@@ -0,0 +1,19 @@
+function largestNumber(num1,num2,num3) {
+    let largestNumber = Number.MIN_SAFE_INTEGER
+
+    if(num1 > largestNumber) {
+        largestNumber = num1;
+    }
+
+    if (num2 > largestNumber) {
+        largestNumber = num2;
+    }
+
+    if (num3 > largestNumber) {
+        largestNumber = num3;
+    }
+
+    console.log(`The largest number is ${largestNumber}.`);
+}
+
+largestNumber(-3, -5, -22.5)
