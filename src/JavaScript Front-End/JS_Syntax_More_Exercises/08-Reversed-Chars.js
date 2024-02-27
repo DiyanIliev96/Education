9c5b81163e2f18dbf456f64a59958fd5d7ca6035
diyaniliev96 <diyan.iliev96@gmail.com>
2024-02-27 13:58:32 +0200

    Task 08 JavaScript homework

---
+++ src/JavaScript Front-End/JS_Syntax_More_Exercises/08-Reversed-Chars.js
@@ -0,0 +1,8 @@
+function reversedChars(firstChar,secondChar,thirdChar) {
+    let charsString = "";
+    charsString += thirdChar + ' '
+    charsString += secondChar + ' '
+    charsString += firstChar + ' '
+
+    console.log(charsString)
+}
