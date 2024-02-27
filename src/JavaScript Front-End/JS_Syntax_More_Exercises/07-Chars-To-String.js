191c9de64c2263a417bdd209b2c22dfd208081ad
diyaniliev96 <diyan.iliev96@gmail.com>
2024-02-27 13:58:07 +0200

    Task 07 JavaScript homework

---
+++ src/JavaScript Front-End/JS_Syntax_More_Exercises/07-Chars-To-String.js
@@ -0,0 +1,8 @@
+function charsToString(firstChar,secondChar,thirdChar) {
+    let charsString = ""
+    charsString += firstChar
+    charsString += secondChar
+    charsString += thirdChar
+    console.log(charsString)
+}
+
