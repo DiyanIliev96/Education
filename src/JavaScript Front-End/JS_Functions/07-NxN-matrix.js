d788233628f4c279760c9c74a0d7a0beae412071
diyaniliev96 <diyan.iliev96@gmail.com>
2024-03-13 23:07:58 +0200

    Task 07 JavaScript functions homework

---
+++ src/JavaScript Front-End/JS_Functions/07-NxN-matrix.js
@@ -0,0 +1,10 @@
+function nXnMatrix(number) {
+    for (let i = 1; i <= number; i++) {
+        let numberToString = number.toString() + ' ';
+     console.log(numberToString.repeat(number))
+    
+    }
+
+}
+
+nXnMatrix(3);
